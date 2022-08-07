5197ea65e0c1015f490834a4dedd484f7a5d1f73
T. K. Stenczel <tks32@cam.ac.uk>
2022-08-07 09:40:18 +0200

    theme & icons

--- docs_rebuild/conf.py
+++ docs_rebuild/conf.py
@@ -6,22 +6,34 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'GAP'
-copyright = '2022, Albert Bartok-Partay, Noam Bernstein, Gabor Csanyi and James Kermode'
-author = 'Albert Bartok-Partay, Noam Bernstein, Gabor Csanyi and James Kermode'
+project = "GAP"
+copyright = (
+    "2010-2022, Albert Bartok-Partay, Noam Bernstein, Gabor Csanyi and James Kermode"
+)
+author = "Albert Bartok-Partay, Noam Bernstein, Gabor Csanyi and James Kermode"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {}
+
+html_logo = "hybrid.png"
+html_favicon = "favicon.ico"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
