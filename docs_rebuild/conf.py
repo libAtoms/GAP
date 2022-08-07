4ad7dff2c850a25dbea0979e2aff5a71e82a3b63
T. K. Stenczel <tks32@cam.ac.uk>
2022-08-07 09:28:36 +0200

    rebuild of docs: quickstart

---
+++ docs_rebuild/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GAP'
+copyright = '2022, Albert Bartok-Partay, Noam Bernstein, Gabor Csanyi and James Kermode'
+author = 'Albert Bartok-Partay, Noam Bernstein, Gabor Csanyi and James Kermode'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs_rebuild/index.rst
@@ -0,0 +1,20 @@
+.. GAP documentation master file, created by
+   sphinx-quickstart on Sun Aug  7 09:27:34 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to GAP's documentation!
+===============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
