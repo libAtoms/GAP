c1263eded36f4b7b9da837ec8a161e1fd90d7426
gabor1 <gc121@cam.ac.uk>
2022-09-06 17:45:33 +0100

    specify logo file

--- doc_src/conf.py
+++ doc_src/conf.py
@@ -40,8 +40,8 @@ html_theme = "sphinx_rtd_theme"
 # documentation.
 html_theme_options = {}
 
-html_logo = "hybrid.png"
-html_favicon = "favicon.ico"
+html_logo = "soap.png"
+html_favicon = "soap.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
