c5e7a95c01c39209d45cda89c2f2a83e37a8a1f1
Sascha Klawohn <sascha.klawohn@warwick.ac.uk>
2023-05-09 16:18:17 +0100

    Add cutoff_ptr

--- gp_fit.f95
+++ gp_fit.f95
@@ -89,6 +89,7 @@ module gp_fit_module
       real(dp), dimension(:,:), allocatable :: sparseX_array
 
       integer, dimension(:), pointer :: config_type_ptr, x_size_ptr
+      real(dp), dimension(:), pointer :: cutoff_ptr
       real(dp), dimension(:,:), pointer  :: dm, x_ptr
 
       character(len=STRING_LENGTH) :: my_sparse_file
@@ -119,6 +120,7 @@ module gp_fit_module
                x_ptr => this%x
                x_size_ptr => this%x_size
                config_type_ptr => this%config_type
+               cutoff_ptr => this%cutoff
             case default
                call print("Collecting x on a single process for sparsification with MPI.")
                n_x = sum(task_manager%mpi_obj, size(this%config_type), error)
@@ -127,15 +129,18 @@ module gp_fit_module
                   allocate(x_ptr(d, n_x))
                   allocate(config_type_ptr(n_x))
                   allocate(x_size_ptr(n_x))
+                  allocate(cutoff_ptr(n_x))
                else
                   my_sparse_method = GP_SPARSE_SKIP
                   allocate(x_ptr(1, 1))
                   allocate(config_type_ptr(1))
                   allocate(x_size_ptr(1))
+                  allocate(cutoff_ptr(1))
                end if
 
                call gatherv(task_manager%mpi_obj, this%config_type, config_type_ptr, error=error)
                call gatherv(task_manager%mpi_obj, this%x, x_ptr, error=error)
+               call gatherv(task_manager%mpi_obj, this%cutoff, cutoff_ptr, error=error)
 
                if (this%covariance_type == COVARIANCE_BOND_REAL_SPACE) then
                   call gatherv(task_manager%mpi_obj, this%x_size, x_size_ptr, error=error)
@@ -145,6 +150,7 @@ module gp_fit_module
          x_ptr => this%x
          x_size_ptr => this%x_size
          config_type_ptr => this%config_type
+         cutoff_ptr => this%cutoff
       end if
 
       if (my_sparse_method /= GP_SPARSE_SKIP) then
@@ -359,7 +365,7 @@ module gp_fit_module
 
          this%covarianceDiag_sparseX_sparseX = this%covarianceDiag_x_x(this%sparseX_index)
 
-         this%sparseCutoff = this%cutoff(this%sparseX_index)
+         this%sparseCutoff = cutoff_ptr(this%sparseX_index)
 
          if(present(print_sparse_index)) then
             if(len_trim(print_sparse_index) > 0) then
@@ -380,6 +386,7 @@ module gp_fit_module
          deallocate(config_type_ptr)
          deallocate(x_ptr)
          deallocate(x_size_ptr)
+         deallocate(cutoff_ptr)
       end if
 
       if (allocated(this%config_type)) deallocate(this%config_type)
